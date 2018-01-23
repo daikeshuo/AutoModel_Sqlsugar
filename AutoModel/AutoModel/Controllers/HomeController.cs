d1036d878a643641009939d2538c940d508ff970
dadehen <dadehen>
2018-01-23 14:24:54 +0800

    wu

--- AutoModel/AutoModel/Controllers/HomeController.cs
+++ AutoModel/AutoModel/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace AutoModel.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index()
+        public ActionResult Index() 
         {
             return View();
         }
